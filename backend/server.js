870f39fea5057d04da446174dd8f9d5a3b46f8b1
CaiminRO <caiminr01@gmail.com>
2024-12-04 06:36:19 -0800

    feat: mount routers on server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const accountRoutes = require('./routes/accountRoutes');
+const todoRoutes = require('./routes/todoRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 // Setup
 const app = express();
@@ -12,6 +15,9 @@ app.use(express.json());
 app.use(cors());
 
 // Mount Routes
+app.use('/api/account', accountRoutes);
+app.use('/api/todos', todoRoutes);
+app.use('/api/admin', adminRoutes)
 
 // Start server
 app.listen(PORT, () => {
